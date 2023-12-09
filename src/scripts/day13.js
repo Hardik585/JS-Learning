f71a4c82a7d3a150283b16e95e2e072b314bfef4
Hardik585 <Hardikkaushik83126@gmail.com>
2023-12-09 23:48:49 +0530

    Js String concept learn

---
+++ src/scripts/day13.js
@@ -0,0 +1,28 @@
+// var userName = prompt("Enter Your Name :");
+// var age = parseInt(prompt("Enter Your age : "));
+// // One way to write String or we can use '' quote
+// var str = "Helo "+ userName + "You will be "+ parseInt(age +1) +" next Year";
+// // Sec way is Here : 
+// var str2 = `Helo ${userName} You will be ${parseInt(age+2)} Next Two Year`
+// document.write(str);
+// document.write(str2);
+
+// // Dynamic Data Taken for login page 
+// var title = prompt("Enter Your title");
+// var typeform  = prompt("enter Your type ");
+// var form1 =
+// `
+// <p>Title</p>${title}
+// <input type="${typeform}">
+// `;
+// document.write(form1);
+
+// // / is used to print char which i used by compiler
+var srcadd = "d:\\minght.jpg";
+document.write(srcadd);
+
+function changeToUpper(){
+    var ifsc = document.getElementById("text-Value").value ;
+    document.getElementById("text-Value").value = ifsc.toUpperCase();
+}
+
