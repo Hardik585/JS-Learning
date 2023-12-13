8ac33911a6ac20418205636ccad9f7e9e91de478
Hardik585 <Hardikkaushik83126@gmail.com>
2023-12-13 10:56:29 +0530

    learn Regular Expresion and how to verify phone number by regular Expresion

---
+++ src/scripts/day16.js
@@ -0,0 +1,9 @@
+function submitFun(){
+    var textValue = document.getElementById("text-value").value;
+    if(textValue.match(/\+\ /)){
+
+    }
+    else{
+        alert("invalid Number ");
+    }
+}
